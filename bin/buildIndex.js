ad1adaf1604b5f6090e6c8e504a581672945e713
Josh Williams <josh@joshwilliams.com>
2015-06-01 12:39:09 -0700

    move section wrapping logic from client to build script. Fixes #23

---
+++ bin/buildIndex.js
@@ -0,0 +1,27 @@
+var markx = require('markx');
+var env = require('jsdom').env;
+
+
+// convert markdown to HTML, and then 
+// wrap that html in section tags for
+// scrollStory's use
+markx({
+  input: 'README.md',
+  template: 'documentation/index.tmpl'
+}, function(err, html) {
+  if (err) {
+    console.log('something went wrong', err);
+  } else {
+    env(html, function(err, window){
+      var $ = require('jquery')(window);
+
+      $('h2, h1', '.content').each(function(){
+        $(this).nextUntil('h2').wrapAll('<section></section>');
+        $(this).prependTo($(this).next('section'));
+      });
+
+      var html = '<!doctype html>\n' + $('html')[0].outerHTML;
+      console.log(html);
+    });
+  }
+});
